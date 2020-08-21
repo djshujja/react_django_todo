a5578eb83d50a8ebdae2357f45902c30e73c8eef
djshujja <djshujja@gmail.com>
2020-08-21 21:04:11 +0500

    added POST and GET

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: [],
+      activeItem: {
+        id: null,
+        title: '',
+        completed: false,
+      },
+      editing: false
+    }
+    this.fetchTasks = this.fetchTasks.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  };
+
+  componentWillMount() {
+    this.fetchTasks()
+  }
+
+  fetchTasks() {
+    console.log('Fetching...')
+
+    fetch('http://127.0.0.1:8000/api/task-list/')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          todoList: data
+        })
+      )
+  }
+
+  handleChange(e) {
+    var name = e.target.name
+    var value = e.target.value
+    console.log(name)
+    console.log(value)
+
+    this.setState({
+      activeItem: {
+        ...this.state.activeItem,
+        title: value
+      }
+    })
+
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(this.state.activeItem)
+    var url = 'http://127.0.0.1:8000/api/task-create/'
+    
+    fetch(url , {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body:JSON.stringify(this.state.activeItem)
+    }).then((res) => {
+      this.fetchTasks()
+      this.setState({
+        activeItem: {
+        id: null,
+        title: '',
+        completed: false,
+      },
+      })
+    }).catch((err) => console.log("Error: ", err))
+
+}
+
+
+
+
+  render() {
+      var tasks = this.state.todoList
+    return(
+      <>
+    <div>
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChange} value={this.state.activeItem.title} type="text" placeholder="Todo" name="title" />
+        <input type="submit" value="add-todo" />
+      </form>
     </div>
-  );
+    <div>
+      {tasks.map(function(task, index) {
+        return (
+          <div key={index}>
+            <div>
+              <span>{task.title}</span>
+              <button>Add</button>
+              <button>Edit</button>
+
+            </div>
+
+          </div>
+        )
+      })}
+    </div>
+
+      </>
+    )
+  }
+
 }
 
 export default App;
